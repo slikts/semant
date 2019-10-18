59d1f9812255857913ea3e2cef192c27f79ea91f
slikts <dabas@untu.ms>
2019-10-18 16:08:11 +0300

    Updates

---
+++ precache-manifest.7608757e341f332d5a61e7fdc8ad9fe5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a88143f0b01742debcf4b089f6a0ced0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "95ef32c6e07e7ae1fa59",
+    "url": "/static/css/main.5d99ab69.chunk.css"
+  },
+  {
+    "revision": "b89fbb8f0ec4841f0380",
+    "url": "/static/js/2.20e11699.chunk.js"
+  },
+  {
+    "revision": "95ef32c6e07e7ae1fa59",
+    "url": "/static/js/main.0162ec9a.chunk.js"
+  },
+  {
+    "revision": "07537d75c4d775ba8601",
+    "url": "/static/js/runtime-main.c22280d5.js"
+  }
+]);
